2596d636dfcde78b39c77fd2a7b483f3eee44a03
Mauricio Da Re <daremauricio5@gmail.com>
2022-03-15 13:26:36 -0300

    added CORS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors')
+
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandle')
 
 const app= express();
@@ -9,6 +11,18 @@ app.use(express.json());
 
 routerApi(app);
 
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('not allowed'));
+    }
+  }
+}
+// middlewares
+app.use(cors(options));
 app.use(boomErrorHandler);
 app.use(logErrors);
 app.use(errorHandler);
